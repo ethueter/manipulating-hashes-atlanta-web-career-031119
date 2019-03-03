c0ad9de75087fc0ec7f3c2c8be867e553282a55d
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-03 01:26:38 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,8 @@ def first_challenge
     }
   }
 
-  contacts["Freddy Mercury"].each do |a, b|
-    a.each do |x, y|
+  contacts.each do |a, b|
+    b.each do |x, y|
     if x == :favorite_icecream_flavors
       y.delete_if {|z| z == ("strawberry")}
     end 
